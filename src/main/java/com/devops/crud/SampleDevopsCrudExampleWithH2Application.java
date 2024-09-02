4615dc669031b83191aea8a2f1e29d92ebb9dc3d
PAVAN KRISHNA KUPPAM <pavan_krishna.kuppam@mercedes-benz.com>
2024-09-02 15:05:24 +0530

    Initial commit

---
+++ src/main/java/com/devops/crud/SampleDevopsCrudExampleWithH2Application.java
@@ -0,0 +1,13 @@
+package com.devops.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDevopsCrudExampleWithH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDevopsCrudExampleWithH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/crud/SampleDevopsCrudExampleWithH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDevopsCrudExampleWithH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
