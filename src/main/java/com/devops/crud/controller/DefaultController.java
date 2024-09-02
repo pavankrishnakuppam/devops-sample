b559c7683ab8a16ea417b14024695a1e2d0aefd7
PAVAN KRISHNA KUPPAM <pavan_krishna.kuppam@mercedes-benz.com>
2024-09-02 16:02:59 +0530

    Initial commit

---
+++ src/main/java/com/devops/crud/controller/DefaultController.java
@@ -0,0 +1,16 @@
+package com.devops.crud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/devops")
+@RestController
+public class DefaultController {
+
+	
+	@GetMapping("/test")
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
